3e33ee254249687f88cdbd8153b08189a8d5b70c
Grad <gradowski.kam@gmail.com>
2020-10-06 21:38:35 +0200

    wyswietlacz wyswietla temperature i wilgotnosc z dht

--- sterwonik_akwarium_arduino_01/sterwonik_akwarium_arduino_01.ino
+++ sterwonik_akwarium_arduino_01/sterwonik_akwarium_arduino_01.ino
@@ -1,13 +1,20 @@
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1331.h>
+#include <Adafruit_GFX.h> // biblioteka do wyswietlacza
+#include <Adafruit_SSD1331.h> // biblioteka do wyswietlacza
 #include <SPI.h>
+#include "DHT.h"
+
+// definicja pinów wyswietlacza
 #define sclk 13
 #define mosi 11
 #define cs   10
 #define rst  9
 #define dc   8
+
+#define DHT11_PIN 2
+DHT dht;
+
 // Color definitions
-#define  BLACK           0x0000
+#define BLACK           0x0000
 #define BLUE            0x001F
 #define RED             0xF800
 #define GREEN           0x07E0
@@ -15,41 +22,61 @@
 #define MAGENTA         0xF81F
 #define YELLOW          0xFFE0  
 #define WHITE           0xFFFF
+
 Adafruit_SSD1331 display = Adafruit_SSD1331(cs, dc, rst);
+
+int wilgotnosc  = 0;
+int temperatura = 0;
+int poprzednia_temperatura = 0;
+int poprzednia_wilgotnosc = 0;
+
 void setup() {
   display.begin();
   display.fillScreen(RED);
-  delay(300);
-  display.fillScreen(GREEN);
-  delay(300);
-  display.fillScreen(BLUE);
-  delay(300);
+  delay(200);
   display.fillScreen(BLACK);
-  delay(1000);
   display.setCursor(20,5);
   display.setTextColor(WHITE);
   display.setTextSize(2);
   display.print("Hello");
-  display.setCursor(8,25);
-  display.setTextColor(RED);
-  display.setTextSize(2);
-  display.print("YouTUBE");
-  display.fillRect(10,40, 75, 20, RED);
-  display.setCursor(20,47);
-  display.setTextColor(WHITE);
-  display.setTextSize(1);
-  display.print("Subscribe");
-  display.drawRect(0,0,96,64,WHITE);
-  delay(1000);
-}
+  
+  Serial.begin(9600);
+  dht.setup(DHT11_PIN);
+  
+ }
 void loop() 
 {
-  display.fillRect(10,40, 75, 20, BLACK);
+  poprzednia_temperatura = temperatura;
+  poprzednia_wilgotnosc = wilgotnosc;
+  wilgotnosc = dht_wilgotnosc();
+  temperatura = dht_temperatura();
+  if((poprzednia_temperatura != temperatura or poprzednia_wilgotnosc != wilgotnosc)and (temperatura <= 200 and wilgotnosc <= 100 )){
+  wyswietl_wilgotnosc_temp(wilgotnosc, temperatura);
+}
   delay(1000);
-  display.fillRect(10,40, 75, 20, RED);
-  display.setCursor(20,47);
-  display.setTextColor(WHITE);
-  display.setTextSize(1);
-  display.print("Subscribe");
-  delay(1000);
+
+                
+}
+
+int dht_wilgotnosc(){
+    int wilgotnosc = dht.getHumidity();
+    if (dht.getStatusString() == "OK") return wilgotnosc;
+}
+
+int dht_temperatura(){
+    int temperatura = dht.getTemperature();
+    if (dht.getStatusString() == "OK") return temperatura;
+}
+void wyswietl_wilgotnosc_temp( int wilgotnosc, int temperatura){
+     display.fillScreen(BLUE);
+     display.setTextColor(WHITE);     
+     display.setCursor(10,5);
+     display.print("W="); 
+     display.setCursor(40,5);
+     display.print(wilgotnosc);
+     display.setCursor(10,25);
+     display.print("T="); 
+     display.setCursor(40,25);
+     display.setTextColor(WHITE);
+     display.print(temperatura);  
 }
